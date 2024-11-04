9c310d308ab7561abfb0cc1773d27b18a28b7e8e
ROCKY <riazulrocky@gmail.com>
2024-11-04 23:25:56 +0600

    covid-19 UI - prevention

--- lib/about.dart
+++ lib/about.dart
@@ -12,137 +12,143 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Column(
-            children: [
-              Image.asset("assets/images/about.png"),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(
               children: [
-                Row(
-                  children: [
-                    Text(
-                      "Symptoms",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-
-                SizedBox(height: 18,),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(13),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 20,
-                              color: kActiveShadowColor,
-                            ),
-                          ]
-                      ),
-                      child: Column(
-                        children: [
-                          Image.asset("assets/images/headache.png", height: 85,),
-                          Text("Headache", style: TextStyle(fontWeight: FontWeight.w500),)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      padding: EdgeInsets.all(13),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 20,
-                              color: kActiveShadowColor,
-                            ),
-                          ]
-                      ),
-                      child: Column(
-                        children: [
-                          Image.asset("assets/images/cough.png", height: 85,),
-                          Text("Cough", style: TextStyle(fontWeight: FontWeight.w500),)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      padding: EdgeInsets.all(13),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 20,
-                              color: kActiveShadowColor,
-                            ),
-                          ]
-                      ),
-                      child: Column(
-                        children: [
-                          Image.asset("assets/images/fever.png", height: 85,),
-                          Text("Fever", style: TextStyle(fontWeight: FontWeight.w500),)
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-                SizedBox(height: 25),
-
-                Row(
-                  children: [
-                    Text("Prevention", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                  ],
-                ),
-
-                SizedBox(height: 15),
-
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          height: 136,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
+                Image.asset("assets/images/about.png"),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  const Row(
+                    children: [
+                      Text(
+                        "Symptoms",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+        
+                  SizedBox(height: 18,),
+        
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
                             color: Colors.white,
                             boxShadow: [
                               BoxShadow(
-                                offset: Offset(0, 8),
-                                blurRadius: 24,
-                                color: kShadowColor,
+                                offset: Offset(0, 10),
+                                blurRadius: 20,
+                                color: kActiveShadowColor,
                               ),
-                            ],
-                            image: const DecorationImage(image: AssetImage("assets/images/wear_mask.png"),alignment: Alignment.bottomLeft),
-                          ),
+                            ]
                         ),
+                        child: Column(
+                          children: [
+                            Image.asset("assets/images/headache.png", height: 85,),
+                            Text("Headache", style: TextStyle(fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                      ),
+        
+                      Container(
+                        padding: EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 20,
+                                color: kActiveShadowColor,
+                              ),
+                            ]
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset("assets/images/cough.png", height: 85,),
+                            Text("Cough", style: TextStyle(fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                      ),
+        
+                      Container(
+                        padding: EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 20,
+                                color: kActiveShadowColor,
+                              ),
+                            ]
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset("assets/images/fever.png", height: 85,),
+                            Text("Fever", style: TextStyle(fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+        
+                  SizedBox(height: 25),
+        
+                  Row(
+                    children: [
+                      Text("Prevention", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+        
+                  SizedBox(height: 15),
 
-                      ],
-                    )
-                  ],
-                )
+                  Column(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            height: 136,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(0, 8),
+                                  blurRadius: 24,
+                                  color: kShadowColor,
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset("assets/images/wear_mask.png"),
+                              ],
+                            ),
+                          ),
 
-
-              ],
-            ),
-          )
-        ],
+                        ],
+                      ),
+                    ],
+                  ),
+        
+        
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
